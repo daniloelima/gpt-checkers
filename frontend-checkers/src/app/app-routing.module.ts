cf1f72b8e40f2a37b1d6d70e5170bdb0e8d9c7eb
Fernando Azevedo <fernando.azevedo.peres@gmail.com>
2023-12-03 19:04:34 -0300

    FEATURE: esboço do frontend

---
+++ frontend-checkers/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './components/game/game.component';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'game',
+    component: GameComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
